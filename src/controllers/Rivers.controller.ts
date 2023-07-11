8d2453883eefbb49a54da1aff03d5feced3dea62
Eric Culley <ericulley@gmail.com>
2023-07-10 22:55:38 -0600

    refactored river services

--- src/controllers/Rivers.controller.ts
+++ src/controllers/Rivers.controller.ts
@@ -1,108 +1,38 @@
 import express, { NextFunction, Request, Response } from 'express';
-import { ObjectId, UpdateResult } from 'mongodb';
-import { River, TRiver, ZRiver } from '../models/River.model';
+import {
+    createRiver,
+    deleteRiver,
+    readAllRivers,
+    readRiver,
+    updateRiver,
+} from '../services/River.services';
 
 // Rivers Router (e.g. {hostname}/rivers/...)
 const rivers = express.Router();
 
 // Create A River
 rivers.post('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const reqData = req.body as TRiver;
-        if (!reqData.createdAt) reqData.createdAt = Date.now();
-        if (!reqData.updatedAt) reqData.updatedAt = undefined;
-        const record = await River.insertOne(reqData);
-        if (record) {
-            res.status(200).send(record);
-        } else {
-            res.status(400).json({ message: 'Bad Request' });
-        }
-    } catch (error: any) {
-        res.status(500).send({ error: error.message });
-    }
+    createRiver(req, res, next);
 });
 
 // Get A River
 rivers.get('/:reportId', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const RiverId = req.params.reportId;
-        const record = await River.findOne({ _id: new ObjectId(RiverId) });
-        if (record) {
-            res.status(200).send(record);
-        } else if (!record) {
-            res.status(400).json({ message: 'Record Not Found' });
-        }
-    } catch (error: any) {
-        res.status(500).send({ error: error.message });
-    }
+    readRiver(req, res, next);
 });
 
 // Get All Rivers
 rivers.get('/', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const records = await River.find({ _id: { $exists: true } }, { limit: 10 }).toArray();
-        if (records) {
-            res.status(200).send(records);
-        } else if (!records) {
-            res.status(400).json({ message: 'Records Not Found' });
-        }
-    } catch (error: any) {
-        res.status(500).send({ error: error.message });
-    }
+    readAllRivers(req, res, next);
 });
 
 // Update A River
 rivers.put('/:reportId', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const reqData = req.body as TRiver;
-        const riverId = req.params.reportId;
-        if (!ZRiver.safeParse(reqData).success) {
-            return res.status(400).json({
-                error: 'Bad Request',
-                error_message: 'Missing required properties',
-            });
-        } else {
-            const record: UpdateResult = await River.updateOne(
-                { _id: new ObjectId(riverId) },
-                {
-                    $set: {
-                        name: reqData.name,
-                        date: reqData.date,
-                        hatches: reqData.hatches,
-                        report: reqData.report,
-                        updatedAt: Date.now(),
-                    },
-                }
-            );
-            if (record.acknowledged && record.matchedCount > 0) {
-                res.status(200).send(record);
-            }
-            if (record.acknowledged && record.matchedCount == 0) {
-                res.status(400).json({ message: 'Record Not Found' });
-            } else if (!record) {
-                res.status(400).json({ message: 'Record Not Found' });
-            }
-        }
-    } catch (error: any) {
-        res.status(500).send({ error: error.message });
-    }
+    updateRiver(req, res, next);
 });
 
 // Delete A River
 rivers.delete('/:riverId', async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const riverId = req.params.riverId;
-        const deletedRecord = await River.deleteOne({
-            _id: new ObjectId(riverId),
-        });
-        if (deletedRecord.acknowledged && deletedRecord.deletedCount > 0)
-            res.status(201).json({ message: 'Record Deleted' });
-        if (deletedRecord.acknowledged && deletedRecord.deletedCount == 0)
-            res.status(400).json({ message: 'Record Not Found' });
-        if (!deletedRecord.acknowledged) res.status(400).json({ message: 'Bad Request' });
-    } catch (error: any) {
-        res.status(500).send({ error: error.message });
-    }
+    deleteRiver(req, res, next);
 });
 
 export default rivers;
--- src/services/River.services.ts
+++ src/services/River.services.ts
@@ -1,14 +1,103 @@
-// Create General Report
-export const createRiver = () => {};
+import express, { NextFunction, Request, Response } from 'express';
+import { ObjectId, UpdateResult } from 'mongodb';
+import { River, TRiver, ZRiver } from '../models/River.model';
 
-// Read General Report
-export const readRiver = () => {};
+// Create A River
+export const createRiver = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const reqData = req.body as TRiver;
+        if (!reqData.createdAt) reqData.createdAt = Date.now();
+        if (!reqData.updatedAt) reqData.updatedAt = undefined;
+        const record = await River.insertOne(reqData);
+        if (record) {
+            res.status(200).send(record);
+        } else {
+            res.status(400).json({ message: 'Bad Request' });
+        }
+    } catch (error) {
+        res.status(500).send({ error: error });
+    }
+};
 
-// ReadAll General Reports
-export const readAllRiver = () => {};
+// Read A River
+export const readRiver = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const RiverId = req.params.reportId;
+        const record = await River.findOne({ _id: new ObjectId(RiverId) });
+        if (record) {
+            res.status(200).send(record);
+        } else if (!record) {
+            res.status(400).json({ message: 'Record Not Found' });
+        }
+    } catch (error) {
+        res.status(500).send({ error: error });
+    }
+};
 
-// Update General Report
-export const updateRiver = () => {};
+// ReadAll Rivers
+export const readAllRivers = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const records = await River.find({ _id: { $exists: true } }, { limit: 10 }).toArray();
+        if (records) {
+            res.status(200).send(records);
+        } else if (!records) {
+            res.status(400).json({ message: 'Records Not Found' });
+        }
+    } catch (error) {
+        res.status(500).send({ error: error });
+    }
+};
 
-// Delete General Report
-export const deleteRiver = () => {};
+// Update A River
+export const updateRiver = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const reqData = req.body as TRiver;
+        const riverId = req.params.reportId;
+        if (!ZRiver.safeParse(reqData).success) {
+            return res.status(400).json({
+                error: 'Bad Request',
+                error_message: 'Missing required properties',
+            });
+        } else {
+            const record: UpdateResult = await River.updateOne(
+                { _id: new ObjectId(riverId) },
+                {
+                    $set: {
+                        name: reqData.name,
+                        date: reqData.date,
+                        hatches: reqData.hatches,
+                        report: reqData.report,
+                        updatedAt: Date.now(),
+                    },
+                }
+            );
+            if (record.acknowledged && record.matchedCount > 0) {
+                res.status(200).send(record);
+            }
+            if (record.acknowledged && record.matchedCount == 0) {
+                res.status(400).json({ message: 'Record Not Found' });
+            } else if (!record) {
+                res.status(400).json({ message: 'Record Not Found' });
+            }
+        }
+    } catch (error) {
+        res.status(500).send({ error: error });
+    }
+};
+
+// Delete A River
+export const deleteRiver = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const riverId = req.params.riverId;
+        const deletedRecord = await River.deleteOne({
+            _id: new ObjectId(riverId),
+        });
+        if (deletedRecord.acknowledged && deletedRecord.deletedCount > 0)
+            res.status(201).json({ message: 'Record Deleted' });
+        if (deletedRecord.acknowledged && deletedRecord.deletedCount == 0)
+            res.status(400).json({ message: 'Record Not Found' });
+        if (!deletedRecord.acknowledged) res.status(400).json({ message: 'Bad Request' });
+    } catch (error) {
+        res.status(500).send({ error: error });
+    }
+};
